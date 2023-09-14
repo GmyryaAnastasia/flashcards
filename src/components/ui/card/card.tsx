3b902cf86395f7971dade5b3b691ed41b27428fe
GmyryaAnastasia <nastgm26@gmail.com>
2023-09-14 21:39:02 +0300

    add card

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,9 +1,9 @@
-import s from './cards.module.scss'
-import {ComponentPropsWithoutRef} from "react/index";
+import s from "./card.module.scss"
+import {ComponentPropsWithoutRef, forwardRef} from 'react'
 
-export type CardProps = {} & ComponentPropsWithoutRef<div>
+
+export type CardProps = {} & ComponentPropsWithoutRef<'div'>
 
 export const Card = forwardRef<HTMLDivElement, CardProps>(({className, ...restProps}, ref) => {
-    const classNames = clsx(s.root, className)
-    return <div ref={ref} className={classNames}{...restProps}></div>
+    return <div ref={ref} className={`${s.root} ${className}`} {...restProps}></div>
 })
